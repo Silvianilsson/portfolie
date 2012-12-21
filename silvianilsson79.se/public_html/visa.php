d11559d226889736a33e608804dc0072591c4569
Silvia Nilsson <silvia.nilsson@hotmail.com>
2012-12-21 09:48:28 +0100

    databaser i visa

--- silvianilsson79.se/public_html/visa.php
+++ silvianilsson79.se/public_html/visa.php
@@ -20,19 +20,30 @@ if (!$item)
 $page_title = $item->title;
 
 ?>
-
 <?php require_once('./php/header.php'); ?>
+<?php require_once('./php/contact.php'); ?>
 
-<div class="row-fluid marketing">
-	<div class="span12 portfolio-item">
-		<a href="/show.php?id=<?php echo $item->id ?>">
-			<img src="http://placehold.it/250x250" class="img-polaroid">
-		</a>
-		<p><?php echo nl2br($item->id) ?></p>
-		<p>Kategori: <strong><?php echo $item->title?></strong></p>
-	</div>
-</div>
+<body>
+	<section id="page_container">
+		<section id="head">
+			<section id="head-left">
+				<header>
+					<a href="index.php"><h1>Silvia Nilsson</h1></a>
+				</header>
+			</section>
+			<section id="head-right">
+				<?php require_once('./php/meny.php'); ?>
+			</section>
+		</section>
+		<section>
+			<div>
+				<a href="<?php echo $item->url ?>">
+					<img src="images/spel.jpg">
+				</a>
+				<p><?php echo nl2br($item->title) ?></p>
+				<p>Kategori: <strong><?php echo $item->title?></strong></p>
+			</div>
 
-<p><a href="/kurser.php">&laquo; Tillbaka</a></p>
-
-<?php require_once('php/footer.php'); ?>
+			<p><a href="/kurser.php">&laquo; Tillbaka</a></p>
+		</section>
+		<?php require_once('php/footer.php'); ?>
