66720e4a2114b067f53946ed64cabfa57ab27033
Silvia Nilsson <silvia.nilsson@hotmail.com>
2013-05-05 21:02:14 +0200

    email fixat

--- silvianilsson79.se/public_html/classes/mail.php
+++ silvianilsson79.se/public_html/classes/mail.php
@@ -3,11 +3,12 @@ $item->title = $_POST['title'];
 
 $to      = 'silvia.nilsson@hotmail.com';
 $from    = $_POST['email'];
-$subject = 'name';
+$subject = $_POST['name'];
 $message = $_POST['message'];
-$headers = 'From: silvia.nilsson@hotmail.com' . "\r\n" .
-    'Reply-To: silvia.nilsson@hotmail.com' . "\r\n" .
+$headers = 'From:'.$from . "\r\n" .
+    'Reply-To:'.$from . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, $message, $headers);
+header("Location: /kontakt.php?name=".$subject);
 ?>
